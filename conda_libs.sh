ffe6a73fba6727970d0ad4446a1554ab6e1b2be4
daan <daan@pop-os.localdomain>
2020-03-29 12:33:21 +0200

    italent

---
+++ conda_libs.sh
@@ -0,0 +1,11 @@
+#! /bin/bash
+
+conda install jupyter
+conda install pandas
+conda install numpy
+conda install pyarrow
+conda install sklearn
+conda install seaborn
+conda install matplotlib
+conda install tensorflow
+conda install keras
